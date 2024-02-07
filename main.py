0d9762ba9e4eef838beee15d0b30b508d78bb48a
imsadik <sadikaman4115@gmail.com>
2024-02-06 22:43:45 -0900

    done main

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
+page_data = {6: 10, 17: 10, 18: 10, 16: 10, 11: 10, 5: 10, 61: 10}
+with open('AI_scraped_data.txt', 'w', encoding='utf-8') as file:
+    for category, page_size in page_data.items():
+            for i in range(1, page_size):
+                url = f'https://press.et/?paged={i}&cat={category}'
+                article = requests.get(url)
+                soup = bs(article.content, "html.parser")
+                links = soup.find(class_='entry-title').find_all('a')
 
-from bs4 import BeautifulSoup as bs
-from urllib.request import Request, urlopen
-import requests
+                for link in links:
+                    link_text = link.get_text()
+                    link_href = link['href']
+                    file.write("Head Line:"+ link_text)
+                    #file.write("URL: " + link_href + "\n")
 
-# Open a file in write mode
-with open('scraped_data.txt', 'w', encoding='utf-8') as file:
-    for i in range(1, 40):
-        url = f'https://press.et/?paged={i}&cat=6'
-        article = requests.get(url)
-        soup = bs(article.content, "html.parser")
-        links = soup.find(class_='entry-title').find_all('a')
-        
-        for link in links:
-            link_text = link.get_text()
-            link_href = link['href']
-            file.write("Head Line: " + link_text + "\n")
-            
-            url1 = link_href
-            nav_article = requests.get(url1)
-            nav_soup = bs(nav_article.content, "html.parser")
-            paragraphs = nav_soup.find(class_='entry-content').find_all('p')
-            
-            news_text = ""
-            for p in paragraphs:
-                news_text += str(p.get_text()) + "\n"
-                file.write(p.get_text() + "\n")
+                    nav_article = requests.get(link_href)
+                    nav_soup = bs(nav_article.content, "html.parser")
+                    paragraphs = nav_soup.find(class_='entry-content').find_all('p')
 
-            file.write("\n\n")  # Add some separation between articles
+                    for p in paragraphs:
+                        file.write(p.get_text() + "\n")
+                    file.write("\n\n")
